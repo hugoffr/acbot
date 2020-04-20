d2ce8dd531c37735561673c0ba8f77beb8628bfe
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-04-20 23:46:20 +0100

    add mongoengine connection

--- acbot/__main__.py
+++ acbot/__main__.py
@@ -1,10 +1,20 @@
 import discord
 import configparser
 import os
+import mongoengine as db
 from acbot.acbot import ACBot
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+db.connect(
+    config['db']['name'], 
+    host=config['db']['host'], 
+    port=int(config['db']['port']),
+    username=config['db']['username'],
+    password=config['db']['password'],
+    authentication_source=config['db']['authentication_source']
+)
 
 bot = ACBot(command_prefix=config['bot']['command_prefix'])
 
